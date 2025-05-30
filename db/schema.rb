8119b9be192a772c219353ca6e7114717bc234fb
huzaifaalam1 <huzaifaalam@arizona.edu>
2025-05-30 15:10:16 -0700

    scaffolds made

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_29_215002) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_30_191704) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
