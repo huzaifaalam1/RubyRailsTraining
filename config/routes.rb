9168e89d4cf245a843040b72e785c6da26cdcc43
huzaifaalam1 <huzaifaalam@arizona.edu>
2025-06-22 01:46:03 -0700

    Sign Up functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get "about" => "static_pages#about"
   get "contact" => "static_pages#contact"
   get "signup" => "users#new"
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :users
 end
