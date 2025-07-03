ef51afb249c06c9a9f0dfbf4f74715e432001b04
huzaifaalam1 <huzaifaalam@arizona.edu>
2025-07-03 13:22:09 -0700

    Fixed tests setup and added tests for relationship and micropost models

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password")
+    @micropost = Micropost.new(content: "This is a sample post", user: @user)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  test "content should not be too long" do
+    @micropost.content = "a" * 256
+    assert_not @micropost.valid?
+  end
+
+  test "most recent micropost should appear first" do
+    older_post = Micropost.create!(content: "Old post", user: @user, created_at: 1.day.ago)
+    newer_post = Micropost.create!(content: "New post", user: @user, created_at: 1.hour.ago)
+    assert_equal [newer_post, older_post], @user.microposts.to_a
+  end
 end
--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user1 = User.create!(name: "User One", email: "userone@example.com", password: "password", password_confirmation: "password")
+    @user2 = User.create!(name: "User Two", email: "usertwo@example.com", password: "password", password_confirmation: "password")
+    @relationship = Relationship.new(follower: @user1, followed: @user2)
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "should require a follower_id" do
+    @relationship.follower = nil
+    assert_not @relationship.valid?
+  end
+
+  test "should require a followed_id" do
+    @relationship.followed = nil
+    assert_not @relationship.valid?
+  end
+
+  test "should belong to follower" do
+    assert_equal @user1, @relationship.follower
+  end
+
+  test "should belong to followed" do
+    assert_equal @user2, @relationship.followed
+  end
 end
